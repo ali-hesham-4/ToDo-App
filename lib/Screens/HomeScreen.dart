2a6a067dcf57e7ec65bc7c5768db33b0d08545a7
Ali-Hesham-1 <ali.hesham@egyis.com>
2024-11-01 18:47:38 +0200

    App Intialization

---
+++ lib/Screens/HomeScreen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class Homescreen extends StatelessWidget {
+  static const String routeName = "home_Screen";
+  const Homescreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
